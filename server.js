323271f8efcdcac47fe44866b2268dc8db4d4580
heganjr <jacobhegan@hotmail.com>
2021-07-07 21:20:43 +0800

    file structures added

---
+++ server.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
+
+
+// server.js
+// handlebars - homepage
+// login
+// profile - see all my own posts, edit and delete my own stuff
+//  handlebars - built-in helper #loggedIn (RecSession) - Express-session (cookies)
+// connection to database - SQL - Sequelize
+// db folder to set up database - schema.sql
